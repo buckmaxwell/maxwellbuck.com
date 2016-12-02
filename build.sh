30352db4ce4b61f86fe6143f1c4cae3d2e7e3a36
buckmaxwell <buck.169@osu.edu>
2016-12-02 09:59:08 -0500

    debug

---
+++ build.sh
@@ -0,0 +1,22 @@
+
+
+# Build homepage
+python build_homepage.py
+
+# Turn markdown files to HTML
+for md_filename in site/*.md
+do
+  html_filename=(${md_filename//.md/.html})
+  html_filename=`echo "print '$html_filename'.lower()" | python`
+  echo "entering grippp!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  grip $md_filename --user-content --export --user=buckmaxwell --pass=$1 $html_filename
+  echo "grip complete!!!!!!!!!!!!!!!!!!!!!!!"
+  
+done
+
+# Remove created markdown files -- DON'T do this. They are used in the Mobile View.
+#rm site/*.md
+
+# Build resume
+grip "RESUME.md" --user-content --export --user=buckmaxwell --pass=$1 "site/resume.html"
+
