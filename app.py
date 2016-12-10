0798bfc7198c916ceaa8133b00b2830174830ce0
buckmaxwell <buck.169@osu.edu>
2016-12-10 15:58:55 -0500

    added cookie tracking. testing

--- app.py
+++ app.py
@@ -2,13 +2,117 @@ from flask import Flask
 from flask import request
 from user_agents import parse
 from flask import render_template
+from names import get_first_name as gfn
+from names import get_last_name as gfn
+from uuid import uuid4
+import pytz
+import datetime
+import psycopg2
+import urlparse
 
 
 app = Flask(__name__)
 
+# Constants
+COOKIE_NAME = 'Max_Buck_is_dope'
+utc=pytz.UTC
+FUTURE = 'Wed, 01 Jan 2116 20:43:10 GMT'
+
+
+def get_connection():
+    # create psycopg2 connection
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return conn
+
+
 def is_mobile(user_agent):
 	user_agent = parse(user_agent)
 	return user_agent.is_mobile or user_agent.is_tablet
+
+
+def get_username():
+	name = gfn() + gfn() + gln() + gln()
+	number = str(ri(1111, 9999))
+	return name + number
+
+
+def create_user(headers):
+	"""Creates a user in the database and returns the username"""
+
+	# Collect User Info
+	id = str(uuid4())
+	username = get_username()
+	useragent = headers.get('User-Agent')
+	mobile = is_mobile(useragent)
+	ipaddr = headers.get('X-Forwarded-For', request.remote_addr)
+	'''
+	start_edit = utc.localize(datetime.datetime.utcnow()) +\
+                         datetime.timedelta(seconds=time_to_wait)'''
+
+	# Add User to DB
+	conn = get_connection()
+	cur = conn.cursor()
+	cur.execute("INSERT INTO users (id, username, useragent, mobile, ipaddress) values (%s, %s, %s, %s, %s);",
+	 (id, username, useragent, mobile, ipaddr))
+	conn.commit()
+	conn.close()
+
+	# Return username
+	return username
+
+
+
+def create_event(headers, cookies, page):
+	"""Create an event, creating a user if need one doesn't yet exist.
+	Return a username if a new user was created.
+	"""
+
+	# Get connection
+	conn = get_connection()
+	cur = conn.cursor()
+
+	created_user = False
+
+	# See if user exists
+	username = cookies.get(COOKIE_NAME)
+	if username:
+		# See if user exists (part 2)
+		cur.execute("SELECT username from users WHERE username=%s", (username, ))
+	    try:
+	        username = cur.fetchone()[0]
+	    except:
+	        username = None
+
+	# Create user if not
+	if not username:
+		username = create_user()
+		created_user = True
+
+	# Collect event info
+	id = str(uuid4())
+	time = utc.localize(datetime.datetime.utcnow())
+	referer = headers.get('Referer')
+
+	# Add entry to events
+	cur.execute("INSERT INTO events (id, page, username, time, referer) VALUES (%s, %s, %s, %s, %s)",
+	 (id, page, username, time, referer))
+	conn.commit()
+	conn.close()
+
+	# Return the username (if it's new)
+	if created_user:
+		return username
+	else:
+		return None
+
 
 def return_404(mobile=False):
 	with open("site/404.html", "r") as f:
@@ -37,7 +141,11 @@ def hello():
 			if is_mobile(request.headers.get('User-Agent')):
 				content = make_mobile(content)
 
-		return content, 200, {'Content-Type':'text/html'}
+		u = create_event(request.headers, request.cookies, 'index.html')
+		headers = {'Content-Type':'text/html'}
+		if u:
+			headers['Set-Cookie'] = "{}={}; {}".format(COOKIE_NAME, u, FUTURE)
+		return content, 200, headers
 	except:
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
@@ -50,7 +158,12 @@ def hello2(filename):
 			content = f.read()
 			if is_mobile(request.headers.get('User-Agent')):
 				content = make_mobile(content)
-		return content, 200, {'Content-Type':'text/html'}
+		
+		u = create_event(request.headers, request.cookies, filename)
+		headers = {'Content-Type':'text/html'}
+		if u:
+			headers['Set-Cookie'] = "{}={}; {}".format(COOKIE_NAME, u, FUTURE)
+		return content, 200, headers
 	except:
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
@@ -61,7 +174,12 @@ def hello3(filename):
 		with open("site/images/{}".format(filename), "r") as f:
 			content = f.read()
 		ext = filename.split('.')[1]
-		return content, 200, {'Content-Type':'image/{}'.format(ext)}
+
+		u = create_event(request.headers, request.cookies, filename)
+		headers = {'Content-Type':'image/{}'.format(ext)}
+		if u:
+			headers['Set-Cookie'] = "{}={}; {}".format(COOKIE_NAME, u, FUTURE)
+		return content, 200, headers
 	except:
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
@@ -71,7 +189,12 @@ def hello4(filename):
 		filename = filename + ".css"
 		with open("site/{}".format(filename), "r") as f:
 			content = f.read()
-		return content, 200, {'Content-Type':'text/css'}
+
+		u = create_event(request.headers, request.cookies, filename)
+		headers = {'Content-Type':'text/css'}
+		if u:
+			headers['Set-Cookie'] = "{}={}; {}".format(COOKIE_NAME, u, FUTURE)
+		return content, 200, headers
 	except:
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
@@ -82,7 +205,12 @@ def hello5(filename):
 		filename = filename + ".md"
 		with open("site/{}".format(filename), "r") as f:
 			content = f.read()
-		return content, 200, {'Content-Type':'text/plain'}
+
+		u = create_event(request.headers, request.cookies, filename)
+		headers = {'Content-Type':'text/plain'}
+		if u:
+			headers['Set-Cookie'] = "{}={}; {}".format(COOKIE_NAME, u, FUTURE)
+		return content, 200, headers
 	except:
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
