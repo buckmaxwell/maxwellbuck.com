748ddd97dbdddde7adf9117374ae971afb148ae0
Max Buck <maxwellhigginsbuck@gmail.com>
2018-02-25 16:38:14 -0500

    bug fix app py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def hello3(filename):
 	except:
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
-@app.route("asset/<filename>.css")
+@app.route("/asset/<filename>.css")
 def site_asset(filename):
 	try:
 		filename = filename + ".css"
@@ -204,7 +204,7 @@ def site_asset(filename):
 		return return_404(is_mobile(request.headers.get('User-Agent')))
 
 
-@app.route("static/octicons/<filename>.css")
+@app.route("/static/octicons/<filename>.css")
 def static_octicons(filename):
 	try:
 		filename = filename + ".css"
@@ -221,7 +221,7 @@ def static_octicons(filename):
 
 
 
-@app.route("static/<filename>")
+@app.route("/static/<filename>")
 def static(filename):
 	try:
 		with open("site/static/{}".format(filename), "r") as f:
